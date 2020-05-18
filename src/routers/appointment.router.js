81a00aeefc5c73dfa281912df81d463c2f6faf51
ShantiLund <shanti.cs15@iba-suk.edu.pk>
2020-05-18 12:49:10 +0500

    change query param

--- src/routers/appointment.router.js
+++ src/routers/appointment.router.js
@@ -3,18 +3,18 @@ const ApponitmentController = require('../controllers/appointment.controller.js'
 const router = express.Router();
 
     // Create a new appointment
-    router.post('/appointments/create', ApponitmentController.create);
+    router.post('/appointment/create', ApponitmentController.create);
 
     // Retrieve all appointments
     router.get('/appointments', ApponitmentController.findAll);
 
     // Retrieve a single appointment with appont_id
-    router.get('/courses/:appoint_id', ApponitmentController.findOne);
+    router.get('/appointment/:appoint_id', ApponitmentController.findOne);
 
     // Update a appointment with appont_id
-    router.put('/courses/:appoint_id', ApponitmentController.update);
+    router.put('/appointment/:appoint_id', ApponitmentController.update);
 
     // Delete a appointment with appont_id
-    router.delete('/courses/:appoint_id', ApponitmentController.delete);
+    router.delete('/appointment/:appoint_id', ApponitmentController.delete);
 
     module.exports = router;
